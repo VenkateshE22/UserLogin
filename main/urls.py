f39481729835f6c8ec3900213157976f8d4c0795
Venkatesh Eleswarapu <dell@abhijeet-Latitude-5480gmail.com>
2022-08-17 19:09:40 +0530

    User Login Demo

---
+++ main/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+app_name = "main"   
+
+
+urlpatterns = [
+    path("register/", views.register_request, name="register"),
+    path("login/", views.login_request, name="login"),
+    path("logout/", views.logout_request, name= "logout"),
+    path("",views.index,name="index"),
+    path("password_reset", views.password_reset_request, name="password_reset"),
+    path("password_change/", views.change_password, name="password_change"),
+
+ ]
